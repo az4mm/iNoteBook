fa4777778bc6a070d04c1938dc250d789b73873e
Azam Hussain <azamdrive143@gmail.com>
2026-01-17 16:01:23 +0530

    fix: correct syntax error in root route handler in index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,11 @@ const port = 5000;
 app.use(cors());
 app.use(express.json()); //middleware to use req.body
 
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+}
+
 //available router
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
